ac44860593993d03c82debdbbf9742ee386b4a92
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2023-11-02 14:39:16 +0100

    L08 - OK

---
+++ L08-ertekReferencia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L08_ertekReferencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
