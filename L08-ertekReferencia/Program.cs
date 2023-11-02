963b703c54db9595b37c105e9e30db9f9a7245b7
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2023-11-02 14:43:29 +0100

    L08 - OK

--- L08-ertekReferencia/Program.cs
+++ L08-ertekReferencia/Program.cs
@@ -2,9 +2,58 @@
 {
     internal class Program
     {
+        // érték szerinti paraméter átadás
+        // int, bool, double, stb.
+        static void Dupla(int a)
+        {
+            // a paraméter értéke lemásolódik
+            // itt a másolattal dolgozol
+
+            a = a * 2;
+        }
+
+        // referencia szerinti paraméter átadás
+        // ref vagy out kulcsszó
+        static void Dupla(ref int a)
+        {
+            // a paraméterre mutató referenciával (mutató) dolgozol
+            // átadott eredeti érték is változik
+            a = a * 2;
+        }
+
+
+        // tömbök, string, objektumok esetében
+        // kulcsszó nélkül is ref. szerinti paraméter átadás van
+
+        // nincs ref
+        static void TömbNulláz(int[] a)
+        {
+            // de mégis kinullázzuk a 0. indexű elemet
+            a[0] = 0;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int b = 10;
+            // érték szerinti paraméter meghívás
+            Dupla(b);
+            // kimenet 10 lesz
+            Console.WriteLine(b);
+
+            // referencia szerinti paraméter meghívás
+            Dupla(ref b);
+
+            // kimenet 20 lesz
+            Console.WriteLine(b);
+
+
+            // kezdeti tömb
+            int[] tömb = { 1, 2, 3 };
+            // metódus meghívása
+            TömbNulláz(tömb);
+
+            // kimenet 0 lesz
+            Console.WriteLine(tömb[0]);
+
         }
     }
 }
