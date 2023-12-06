1f7fddf90e9a5d744ec3595a0ffeb21f93e92ed9
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2023-12-06 11:29:40 +0100

    mintaZH2 - ok

---
+++ ZH00-mintaZH2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZH00_mintaZH2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
