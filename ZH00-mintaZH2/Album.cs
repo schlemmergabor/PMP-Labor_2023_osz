666ebeb053e43e6df0ae503092f4311c73e55948
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2023-12-06 13:42:58 +0100

    mintaZH2 - ok - megjegyzéssel

--- ZH00-mintaZH2/Album.cs
+++ ZH00-mintaZH2/Album.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ZH00_mintaZH2
 {
+    // saját enum készítése
     enum Adathordozo
     {
         Kazetta, 
@@ -18,43 +19,64 @@ namespace ZH00_mintaZH2
         string előadó;
         string cim;
         Adathordozo adathordozoTipusa;
+        // listában tárolom a zeneszámokat
         List<Zeneszam> zeneSzamok;
 
+        // .Count() adja vissza a lista elemszámát
         public int ZenekSzama { get { return zeneSzamok.Count(); }  }
-
 
         private Zeneszam ZeneszamFeldolgozas(string s)
         {
+            // feldarabolom ;-nál
             string[] darab = s.Split(';');
+
             string cim = darab[0];
             string eloadok = darab[1];
             int hossz = int.Parse(darab[2]);
 
+            // új példányt, objektumot létrehozok és egyből vissza is adom
             return new Zeneszam(cim, eloadok, hossz);
         }
 
         private void FelveteltHozzaad(Zeneszam z)
         {
+            // csak ennyi, hogy elrejtsem, hogy tömbbel vagy listával oldottam meg
+            // a zeneszámok tárolását
             zeneSzamok.Add(z);
+
+            // itt tömb esetében hosszabb a kód
+            // temp tömb -> tömb+1 mérettel
+            // átmásol tömb-ből temp-be
+            // temp utolsó indexére új zeneszam
+            // tömb = temp
         }
 
         public Album(string előadó, string cim, string file)
         {
             this.előadó = előadó;
             this.cim = cim;
+            
+            // lista létrehozása
             zeneSzamok = new List<Zeneszam>();
-            // milyen legyen az adathordozó típusa?
-
+            
+            
+            // Milyen legyen az adathordozó típusa?
+            // Kérdezzük meg a felhasználót (lásd 3. feladat Main...)
             Console.Write("Milyen legyen az adathordozó típusa? Kazetta/CD/Bakelit/Stream ");
 
+            // szöveg beolvasása
             string be = Console.ReadLine();
 
+            // saját Enum-ba Parseolás
             adathordozoTipusa = (Adathordozo)Enum.Parse(typeof(Adathordozo), be);
 
+            // file beolvasása egy tömbbe
             string[] fileSorok = File.ReadAllLines(file);
+            // végig megyek a beolvasott tömbön
             foreach (string item in fileSorok)
             {
-                zeneSzamok.Add(ZeneszamFeldolgozas(item));
+                // hozzáadom a feldolgozott felvételt
+                FelveteltHozzaad(ZeneszamFeldolgozas(item));
             }
             
         }
@@ -63,36 +85,56 @@ namespace ZH00_mintaZH2
 
         private string LegrovidebbFelvetel()
         {
+            // minimum kiválasztásos tétel
+            // kezdeti minimum indexe az első
             int minIndex = 0;
+            
+            // másodiktól végignézem
             for (int i = 1; i < zeneSzamok.Count; i++)
             {
+                // ha az i-edik rövidebb, akkor index elmentése
                 if (zeneSzamok[i].Hossz < zeneSzamok[minIndex].Hossz) minIndex = i;
             }
+            // legkisebb címének returnja
             return zeneSzamok[minIndex].Cim;
         }
 
         public int AdottHosszFelett(int mp)
         {
+            // megszámlálás tétele
+
+            // kezdetben 0 db
             int db = 0;
+            // végig az egészen
             for (int i = 0; i < zeneSzamok.Count; i++)
             {
+                // ha hossz nagyobb, akkor db=db+1
                 if (zeneSzamok[i].Hossz > mp) db++;
             }
+
             return db;
         }
 
         public string[] SzerzoKivalogat(string s)
         {
+            // megszámlálás tétele
             int db = 0;
             for (int i = 0; i < zeneSzamok.Count; i++)
             {
+                // Szerzo (string)-ben ha van s, akkor db=db+1;
                 if (zeneSzamok[i].Szerzo.Contains(s)) db++;
             }
 
+            // db-nyi számot számoltam meg
             string[] temp = new string[db];
+            // j-edik szám címét mentettem el
             int j = 0;
+            // végig megyek az összes zeneszámon
             for (int i = 0; i < zeneSzamok.Count; i++)
             {
+                // ha a s-t tartalmazza a szerző string, akkor
+                // temp-be hozzáadom
+                // és j-t megnövelem
                 if (zeneSzamok[i].Szerzo.Contains(s)) temp[j++] = zeneSzamok[i].Cim;
             }
             return temp;
@@ -100,12 +142,17 @@ namespace ZH00_mintaZH2
 
         public string AlbumStatisztika()
         {
+            // temp string-et állítok elő
+            // $"" -ben a $ jel azért kell, hogy a változókat, metódusokat
+            // megtudjam hívni { } jelek között
+
             string temp =$"{előadó} - {cim} ({adathordozoTipusa})\n\n";
 
             temp += $"Dalok száma: {this.ZenekSzama}\n";
 
             temp += $"A legrövidebb dal címe: {LegrovidebbFelvetel()}\n";
 
+            // átlag számítása
             int összHossz = 0;
             foreach (Zeneszam item in zeneSzamok)
             {
@@ -116,13 +163,13 @@ namespace ZH00_mintaZH2
 
             temp += $"A dalok átlagos hossza: {ts.ToString("mm':'ss")}\n\n";
 
+            // dalok listájának kiírása sorrendben
             for (int i = 0; i < zeneSzamok.Count; i++)
             {
                 string[] db = zeneSzamok[i].Szerzo.Split(',');
-
+                
+                // i+1 azért, mert 1. sorszám van
                 temp += $"{i + 1}. {zeneSzamok[i].Cim} ({db.Length} szerző)\n";
-
-
             }
             return temp;
         }
--- ZH00-mintaZH2/Program.cs
+++ ZH00-mintaZH2/Program.cs
@@ -6,8 +6,10 @@
       
         static void Main(string[] args)
         {
+            // Album objektum létrehozása
             Album a = new Album("Taylor Swift", "Midnights", @"..\..\..\taylor_swift-midnights.txt");
             
+            // Albumstatisztika metódus meghívása
             Console.WriteLine(a.AlbumStatisztika());
             ;
 
